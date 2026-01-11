eda5d7737a8f279550a4b23c8a5839666775aefc
shweepps <shweepps@gmail.com>
2026-01-11 14:13:57 +0200

    week 1 done

--- week01/teach/Divisors.cs
+++ week01/teach/Divisors.cs
@@ -16,9 +16,21 @@ public static class Divisors {
     /// </summary>
     /// <param name="number">The number to find the divisor</param>
     /// <returns>List of divisors</returns>
-    private static List<int> FindDivisors(int number) {
-        List<int> results = new();
-        // TODO problem 1
-        return results;
+   private static List<int> FindDivisors(int number)
+{
+    List<int> results = new();
+
+    // Loop from 1 to number - 1
+    for (int i = 1; i < number; i++)
+    {
+        // If 'i' divides number evenly, it's a divisor
+        if (number % i == 0)
+        {
+            results.Add(i);
+        }
     }
+
+    return results;
+}
+
 }
